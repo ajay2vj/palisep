407da90b995413538522b255fc339d0b52fc576b
ajay2vj <vjajay98@gmail.com>
2023-12-10 07:49:44 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+import TabPanel from "./components/tabpanel";
+import "./App.css";
+
+function App() {
+	return (
+		<Box>
+			<TabPanel />
+		</Box>
+	);
+}
+
+export default App;
